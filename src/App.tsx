24442b78a677ad13986d3ca80b00f17a43d6e38c
Joshua Silveous <JoshSilveous@gmail.com>
2022-10-03 16:46:17 -0400

    Added hover effects and more props for configuration. Clarified tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,33 @@ import './App.css';
 import { JSelect } from './components';
 
 function App() {
+    
     return (
         <div className="App">
             hello
             <div className="JSelectContainer-1">
                 <JSelect
+                    key="1"
                     values={['value 1', 'value 2', 'value 3']}
                     defaultValueIndex={2}
+                    backgroundColor="var(--background)"
+                    backgroundColorHover="red"
+                />
+            </div>
+            <div className="JSelectContainer-2">
+                <JSelect
+                    key="2"
+                    values={['value 1', 'value 2', 'value 3', 'value 4', 'value 5']}
+                    defaultValueIndex={2}
+                    backgroundColor="var(--background-light)"
+                />
+            </div>
+            <div className="JSelectContainer-3">
+                <JSelect
+                    key="3"
+                    values={['value 1', 'value 2', 'value 3', 'value 4', 'value 5']}
+                    defaultValueIndex={2}
+                    backgroundColor="var(--text)"
                 />
             </div>
         </div>
--- src/components/JSelect/JSelect.tsx
+++ src/components/JSelect/JSelect.tsx
@@ -1,12 +1,15 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react'
+import React, { useState, useRef, useLayoutEffect, useMemo } from 'react'
+import { arrayBuffer } from 'stream/consumers';
 import './JSelect.css'
 
 export interface JSelectProps {
     values: string[] | number[];
     defaultValueIndex: number;
+    backgroundColor: string;
+    backgroundColorHover?: string;
 }
 
-function JSelect({ values, defaultValueIndex }: JSelectProps) {
+function JSelect({ values, defaultValueIndex, backgroundColor, backgroundColorHover = "" }: JSelectProps) {
 
     const [current, setCurrent] = useState(defaultValueIndex)
     const [dropdownOpen, setDropdownOpen] = useState(false)
@@ -14,25 +17,41 @@ function JSelect({ values, defaultValueIndex }: JSelectProps) {
     // Gets the height of the JSelect's parent container,
     // and assigns it to defaultHeight
     const jselectRef = useRef<HTMLButtonElement>(null)
-    const [defaultHeight, setDefaultHeight] = useState<number>(0)
-    useEffect(() => {
-        if (jselectRef.current !== null && jselectRef.current.offsetParent !== null) {
-            setDefaultHeight(jselectRef.current.offsetParent.clientHeight!)
+    let [defaultHeight, setDefaultHeight] = useState<number>(0)
+    useLayoutEffect(() => {
+        if (jselectRef.current !== null) {
+            setDefaultHeight(jselectRef.current.clientHeight!)
         }
     }, [])
-
     function determineButtonHeight() {
         // Prevents slide-down animation due to pageload defaultHeight being 0
         if (defaultHeight === 0) {
             return 'inherit'
         } else {
             if (dropdownOpen) {
-                return defaultHeight * 10
+                return 'fit-content'
             } else {
                 return defaultHeight
             }
         }
     }
+
+
+    const valuesDisplay = values.map((item, index, array) => {
+        return (
+            <div className="jselect__valuesitem"
+                style={{
+                    paddingBottom: index + 1 == array.length ? '15px' : '2'
+                }}>
+                {item}
+            </div>
+        )
+    })
+
+    // console.log(jselectRef.current?.childNodes)
+
+    // let heightCalc = 0;
+    // jselectRef.current?.childNodes.forEach(item => console.log(item))
 
     return (
         <>
@@ -41,10 +60,10 @@ function JSelect({ values, defaultValueIndex }: JSelectProps) {
                 onClick={() => { setDropdownOpen(prev => !prev) }}
                 ref={jselectRef}
                 style={{
-                    backgroundColor: 'var(--background)',
+                    backgroundColor: backgroundColor,
                     height: determineButtonHeight(),
-                    borderRadius: defaultHeight * 0.5,
-                    transition: defaultHeight === 0 ? '' : 'background-color 0.2s ease, height 0.4s ease'
+                    // borderRadius: defaultHeight * 0.5,
+                    transition: defaultHeight === 0 ? '' : 'background-color 0.2s ease, height 0.4s ease, filter 0.2s ease'
                 }}
             >
                 <div className="jselect__current" style={{
@@ -52,7 +71,13 @@ function JSelect({ values, defaultValueIndex }: JSelectProps) {
                 }}>
                     {values[current]}
                 </div>
+                <div className="jselect__divider" />
+                {valuesDisplay}
+                <div className="jselect__backgroundDiv" />
             </button>
+            
+            
+
 
 
         </>
